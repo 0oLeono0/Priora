840a5241be482697b1b7b9a009f0b0079d5c8723
unknown <lyonya_ivanovskij@mail.ru>
2024-05-17 00:55:40 +0300

    ended main page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const select1 = new ItcCustomSelect('#select-1');
+    const boxes = Array.from(document.querySelectorAll(".accardion__box"));
+
+    function boxHandler(e) {
+        e.preventDefault();
+        let currentBox = e.target.closest(".accardion__box");
+        let currentContent = e.target.nextElementSibling;
+        currentBox.classList.toggle("active");
+        if (currentBox.classList.contains("active")) {
+            currentContent.style.maxHeight = currentContent.scrollHeight + "px";
+        } else {
+            currentContent.style.maxHeight = 0
+        }
+    }
+
+    boxes.forEach((box) => {
+        box.addEventListener("click", boxHandler);
+    })
 })
