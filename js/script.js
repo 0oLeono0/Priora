06d02231c1da0df34d93f991f8aadf8d01e8597a
unknown <lyonya_ivanovskij@mail.ru>
2024-05-19 20:29:01 +0300

    Added styles page

--- js/script.js
+++ js/script.js
@@ -17,4 +17,17 @@ document.addEventListener('DOMContentLoaded', () => {
     boxes.forEach((box) => {
         box.addEventListener("click", boxHandler);
     })
+
+    // Получаем все кнопки с классом "accardion__btn"
+    const buttons = document.querySelectorAll('.accardion__btn');
+
+    // Добавляем обработчик событий для каждой кнопки
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            // Получаем значение атрибута data-style
+            const style = this.getAttribute('data-style');
+            // Перенаправляем пользователя на страницу styles.html с параметром стиля
+            window.location.href = `styles.html?style=${style}`;
+        });
+    });
 })
---
+++ js/styles.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const select1 = new ItcCustomSelect('#select-1');
+    // Функция для показа блока по стилю и изменения стиля кнопки
+    function showStyleBlock(style) {
+        // Скрываем все блоки
+        document.querySelectorAll('.main__about').forEach(block => {
+            block.classList.remove('active');
+        });
+        // Показываем блок с соответствующим id
+        const activeBlock = document.getElementById(style);
+        if (activeBlock) {
+            activeBlock.classList.add('active');
+        }
+
+        // Сбрасываем стиль всех кнопок
+        document.querySelectorAll('.main__btn').forEach(button => {
+            button.classList.remove('active');
+        });
+        // Устанавливаем стиль активной кнопки
+        const activeButton = document.querySelector(`.main__btn[data-style="${style}"]`);
+        if (activeButton) {
+            activeButton.classList.add('active');
+        }
+    }
+
+    // Проверка параметров URL
+    const params = new URLSearchParams(window.location.search);
+    const styleParam = params.get('style');
+    if (styleParam) {
+        showStyleBlock(styleParam);
+    }
+
+    // Добавляем обработчики событий для кнопок
+    const buttons = document.querySelectorAll('.main__btn');
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            const style = this.getAttribute('data-style');
+            showStyleBlock(style);
+            // Обновляем URL без перезагрузки страницы
+            window.history.pushState({}, '', `?style=${style}`);
+        });
+    });
+})
